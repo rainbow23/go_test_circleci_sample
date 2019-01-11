4d881cc7d654a76a7c450e6150e5953dc780be63
rainbow23 <info@rainbow23.jp>
2019-01-11 19:20:41 +0000

    first commit

---
+++ sample01_test.go
@@ -0,0 +1,14 @@
+// Package sample01 provides
+package sample01
+
+import (
+	"testing"
+)
+
+func TestHelloWorld(t *testing.T) {
+	actual := HelloWorld("hoge")
+	expected := "hello world, hoge"
+	if actual != expected {
+		t.Errorf("actual %v\nwant %v", actual, expected)
+	}
+}
